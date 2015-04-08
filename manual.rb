6d45d3412b5da2f138a77f2bf7ac4f6d29ddc037
ajfaraday <andrewfaraday@hotmail.co.uk>
2015-04-08 22:39:37 +0100

    added 'play all' button to manual input

--- manual.rb
+++ manual.rb
@@ -5,9 +5,16 @@ Shoes.app do
   stack do 
     title 'TTM Shoes'
   end 
+  @play_all = button 'Play All'
+
+  @inputs = []
 
   # TODO set these up to use config file
-  ManualInput.new(self,PureData.new('3939'))
-  ManualInput.new(self,PureData.new('3838'))
+  @inputs << ManualInput.new(self,PureData.new('3939'))
+  @inputs << ManualInput.new(self,PureData.new('3838'))
+
+  @play_all.click do
+    @inputs.each{|i|i.play_field}
+  end
 
 end
